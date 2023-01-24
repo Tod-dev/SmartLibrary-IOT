51edf7440eff9029a5117f84fa9a5b40fc4652c5
Marco Todaro <marcoposta59@gmail.com>
2023-01-24 16:30:43 +0100

    bot start

---
+++ bot/bot.py
@@ -0,0 +1,61 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from config import BOTKEY,SERVER_URL
+import requests
+from datetime import datetime
+
+#LOGGING
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+def getOperations():
+    return ['prenota','ritira','consegna']
+
+#COMANDI
+def start(update, context):
+    """Send a message when the command /now is issued."""
+    username = update.message.chat.username
+    update.message.reply_text('Hello, {}!\n/help for documentation.'.format(username))
+
+def help(update, context):
+    """Send a message when the command /now is issued."""
+    operazioniDesc = map(lambda x: '/{} , {} un libro.'.format(x,x), getOperations())
+    text = '\n'.join(operazioniDesc)
+    update.message.reply_text(text)
+
+#TODO
+def prenota(update, context):
+    """Send a message when the command /now is issued."""
+    #r = requests.get('{}/totems?nomeLibro={}'.format(SERVER_URL,nome))
+    update.message.reply_text('Not yet implemented :(')
+#TODO
+def ritira(update, context):
+    """Send a message when the command /now is issued."""
+    update.message.reply_text('Not yet implemented :(')
+#TODO
+def consegna(update, context):
+    """Send a message when the command /now is issued."""
+    update.message.reply_text('Not yet implemented :(')
+
+#START BOT
+def startBot():
+    global updater
+    """Start the bot."""
+    updater = Updater(BOTKEY, use_context=True)
+    dp = updater.dispatcher
+
+    #HANDLERS COMANDI
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("prenota", prenota))
+    dp.add_handler(CommandHandler("ritira", ritira))
+    dp.add_handler(CommandHandler("consegna", consegna))
+    
+    updater.start_polling()
+    return updater
+
+#MAIN
+if __name__ == '__main__':
+    updater = startBot()
+    updater.idle()
