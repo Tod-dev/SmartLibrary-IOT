9bb336f488732645f0fac72705fe1c2b1c847968
Marco Todaro <marcoposta59@gmail.com>
2023-02-04 11:04:36 +0100

    new book

--- db/02_INSERT.sql
+++ db/02_INSERT.sql
@@ -4,7 +4,7 @@ insert into generi values
 	(3,'Thriller'),
 	(4,'Narrativa');
 
-insert into autori values(1, 'Emily Bronte'), (2, 'Harper Lee'), (3, 'Niccolò Ammaniti'), (4, 'Tiziano Sclavi');
+insert into autori values(1, 'Emily Bronte'), (2, 'Harper Lee'), (3, 'Niccolò Ammaniti'), (4, 'Tiziano Sclavi'),(5,'Oscar Wilde');
 	
 insert into totems values  (1, 'qr-1', 'https://goo.gl/maps/ismPzxi1eAWPbTan7','Biblioteca Panizzi, Via Luigi Carlo Farini, Reggio Emilia, RE'), 
 (2, 'qr-2','https://goo.gl/maps/P66FdLfTZEAy2Bzr8' ,'Unimore - V.le Timavo, Viale Timavo, Reggio Emilia, RE');
@@ -17,7 +17,8 @@ insert into libri values
 	(1,'Dylan Dog - I Nuovi Barbari', 2, 4, 1, 'https://shop.sergiobonelli.it/resizer/-1/-1/true/ce17ac40d4525d60d687ab1e318d70b9.jpg--i_nuovi_barbari.jpg','04e92c54'),
 	(2,'Cime Tempestose', 1, 1, 2, 'https://m.media-amazon.com/images/I/71U4qfrRjtL.jpg','04eb2c54'),
 	(3,'Il Buio oltre la siepe', 3, 2, 3, 'https://m.media-amazon.com/images/I/81QJbER-cbL.jpg','04df2c54'),
-	(4,'Io non ho paura', 4, 3, 4, 'https://m.media-amazon.com/images/I/41+WZIs5YSL._SX322_BO1,204,203,200_.jpg','04e12c54');
+	(4,'Io non ho paura', 4, 3, 4, 'https://m.media-amazon.com/images/I/41+WZIs5YSL._SX322_BO1,204,203,200_.jpg','04e12c54'),
+	(5,'Il ritratto di Dorian Gray', 3, 5, 5, 'https://www.myliberty.it/public/libri/38.jpg','04e22c54');
 	
 delete from prestiti;
 /*insert into prestiti values
